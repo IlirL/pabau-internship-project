3f000874c5fcee78d55f0ec80f82c32ed4f7d880
IlirL <ilir.limani@live.com>
2021-12-10 02:46:55 +0100

    add updates

--- src/Components/GetPastMissions.js
+++ src/Components/GetPastMissions.js
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from 'react'
 import {useQuery, gql} from "@apollo/client";
 import {Load_PastMissions} from '../GraphQL/Queries';
 import PostPastMission from './PostPastMission';
-import "./GetPastMissions.css"
 import { Container, Grid, Typography } from '@mui/material';
 import {Route} from 'react-router-dom'
 function GetPastMissions() {
@@ -13,26 +12,21 @@ function GetPastMissions() {
     const [pastMissions, setPastMissions] = useState([]);
 
     useEffect(()=>{
-        // console.log("data = ", data);
         if(data)
         {
             console.log("We are in with data = ", data);
             setPastMissions(data.launchesPast);
         }
-            
-        console.log(pastMissions);//we are getting the data but its console.log is being rendered faster
     }, [data]);
 
     return (
      
         <Container>
-            
-             {/* <center><h1 id = "title">Past Spacex Missions</h1></center> */}
-             <Typography variant = "h3" align = 'center' component = "div" nowrap = "false" gutterBottom>Past Spacex Missions</Typography>
+            <Typography variant = "h3" align = 'center' component = "div" nowrap = "false" gutterBottom>
+                Past Spacex Missions</Typography>
            <Grid container spacing = {5}>
             {
                 pastMissions && pastMissions.map((pastMission, index) =>{
-                    // console.log("ilir = ", {...pastMission});
                     return <Grid item key = {index} xs = {12} md = {6} lg = {4} >
                             <PostPastMission pastMission = {{...pastMission}}/>
                         </Grid>
--- src/GraphQL/Queries.js
+++ src/GraphQL/Queries.js
@@ -23,28 +23,6 @@ export const Load_PastMissions = gql`
 
 `;
 
-export const Load_missionDetail = gql`
-{
-  launchesPast(limit: 20) {
-    mission_name
-    launch_date_local
-    links {
-      article_link
-      video_link
-      wikipedia
-      reddit_campaign
-      flickr_images
-    }
-    rocket {
-      rocket_name
-      rocket_type
-    }
-    details
-    id
-  }
-}
-
-`
 
 
 
